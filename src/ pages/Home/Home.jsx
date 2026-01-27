49cabf40738d7e379a1a2f40ac1d74892013d160
Rex Umeh <rex@Rexs-MacBook-Pro.local>
2026-01-27 16:56:38 +0100

    added the footer

--- src/ pages/Home/Home.jsx
+++ src/ pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import hero_title from "../../assets/hero_title.png";
 import play_icon from "../../assets/play_icon.png";
 import info_icon from "../../assets/info_icon.png";
 import TitleCards from "../../components/TitleCards/Titlecards";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -33,11 +34,13 @@ const Home = () => {
           <TitleCards />
         </div>
       </div>
-      <div className="more-cards"></div>
-      <TitleCards title={"bluckbuster Movie"} />
-      <TitleCards title={"only on Netflix"} />
-      <TitleCards title={"upcoming"} />
-      <TitleCards title={"Top pics for You"} />
+      <div className="more-cards">
+        <TitleCards title={"bluckbuster Movie"} />
+        <TitleCards title={"only on Netflix"} />
+        <TitleCards title={"upcoming"} />
+        <TitleCards title={"Top pics for You"} />
+      </div>
+      <Footer />
     </div>
   );
 };
