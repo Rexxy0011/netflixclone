826e89f28d0fadccd95532018bf42a1dec0b1b35
rexxy0011 <ifeanyiumeh47@gmail.com>
2026-01-31 16:36:32 +0100

    closed gaps

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./ pages/Login/Login";
 import Player from "./ pages/Player/Player";
 import { onAuthStateChanged } from "firebase/auth";
-
 import { auth } from "./firebase";
-
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
